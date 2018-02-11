2d712a7ef4505ed653f54846fef9f1d4e941ecc1
dylanhobbs <dhobbs@tcd.ie>
2018-02-11 14:13:22 +0000

    Back end validation and sessions

--- app/models/user.js
+++ app/models/user.js
@@ -1,13 +1,77 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 var bcrypt   = require('bcrypt-nodejs');
+var titlize      = require('mongoose-title-case');
+var validate = require('mongoose-validator');
 
+// ****************************************************************************
+// *                      Hardcore backend validation with RegEx                    *
+// ****************************************************************************
+// Name validator in form [Dylan Hobbs or Mary-Walter-Smith]
+var nameValidator = [
+	validate({
+		validator: 'matches',
+		arguments: /^(([a-zA-Z]{2,20})+[ ]+([a-zA-Z-]{2,20})*)+$/,
+		message: 'Name must be at least 2 charecters per name, max 20. \nSpace seperarated. \nAllowed {a-z, A-Z, "-"}. \n Eg. John Doe or Susan Walter-Smith'
+	}),
+	validate({
+	  validator: 'isLength',
+	  arguments: [4, 25],
+	  message: 'Name should be between {ARGS[0]} and {ARGS[1]} characters'
+	})
+];
+
+// Email Validator in form dhobbs@tcd.ie. You know what an email looks like. I feel silly
+var emailValidator = [
+	validate({
+		validator: 'isEmail',
+		message: 'Not a valid email address.'
+	}),
+	validate({
+	  validator: 'isLength',
+	  arguments: [3, 25],
+	  message: 'Email should be between {ARGS[0]} and {ARGS[1]} characters'
+	}),
+];
+
+// Username Validator in form blahblah69 or XxN05c0pe420xX just keep it alpha / numeric and we G
+var usernameValidator = [
+	validate({
+	  validator: 'isLength',
+	  arguments: [3, 25],
+	  message: 'Username should be between {ARGS[0]} and {ARGS[1]} characters'
+	}),
+	validate({
+	  validator: 'isAlphanumeric',
+	  message: 'Username must contain letters and numbers only'
+	})
+];
+
+// Password Validator. 8-40 charecters. At least one capital letter, one symbol and one number
+var passwordValidator = [
+	validate({
+		validator: 'matches',
+		arguments: /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,40}$/,
+		message: 'Password must be longer than 8 charecrers and contain at least one symbol, upper case letter and number'
+	}),
+	validate({
+	  validator: 'isLength',
+	  arguments: [8, 40],
+	  message: 'Password should be between {ARGS[0]} and {ARGS[1]} characters'
+	})
+];
+
+// ****************************************************************************
+// *                             Start User Schema                            *
+// ****************************************************************************
 var UserSchema = new Schema({
-	username: {type: String, lowercase: true, required: true, unique: true},
-	password: {type: String, required: true},
-	email:    {type: String, lowercase: true, required: true, unique: true}
+	name:     {type: String, required: true, validate: nameValidator},
+	username: {type: String, lowercase: true, required: true, unique: true, validate: usernameValidator},
+	password: {type: String, required: true, validate: passwordValidator},
+	email:    {type: String, lowercase: true, required: true, unique: true, validate: emailValidator}
 });
 
+// Middleware for hashing passwords
 UserSchema.pre('save', function(next){
 	var user = this;
 	bcrypt.hash(user.password, null, null, function(err, hash){
@@ -16,5 +80,16 @@ UserSchema.pre('save', function(next){
 		next();
 	});
 });
+
+// Mongoose plugin to make names automatically "Title Case"
+UserSchema.plugin(titlize, {
+  paths: [ 'name' ]
+});
+
+// Middleware for password validation
+// Using a custom function for the MongoDB
+UserSchema.methods.comparePasswords = function(password){
+	return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,26 +1,124 @@
 // ****************************************************************************
 // *                              Backend Routes                              *
 // ****************************************************************************
-var User = require('../models/user')
+var User    = require('../models/user')
+var jwt     = require('jsonwebtoken');
+var secret  = 'sabaton';
 
 module.exports = function(router){
+	// http://localhost:8080/api/users
+	// USER REGISTRATION ROUTE
 	router.post('/users', function(req, res){
 		var user = new User();
 		user.username = req.body.username;
 		user.password = req.body.password;
 		user.email    = req.body.email;
+		user.name     = req.body.name;
 
-		if(req.body.username == null || req.body.username == '' || req.body.password == null || req.body.password == '' || req.body.email == null || req.body.email == ''){
-			res.send("Ensure username, email and password are provided");
+		if(req.body.username == null || req.body.username == '' || req.body.password == null || req.body.password == '' || req.body.email == null || req.body.email == '' || req.body.name == null || req.body.name == ''){
+			res.json({success: false, message: 'Ensure username, email, name and password are provided'});
 		} else {
 			user.save(function(err){
 				if(err){
-					res.send('Username or Email already exists');
+					// Handles all validation errors
+					if(err.errors != null){ 
+						if(err.errors.name){
+							res.json({success: false, message: err.errors.name.message});
+						} else if(err.errors.email){
+							res.json({success: false, message: err.errors.email.message});
+						} else if(err.errors.username){
+							res.json({success: false, message: err.errors.username.message});
+						} else if(err.errors.password){
+							res.json({success: false, message: err.errors.password.message});
+						} else {
+							res.json({success: false, message: err});
+						}
+					} else if(err){
+						// Duplicate username or email
+						if(err.code = 11000){
+							// Dup username
+							if(err.errmsg[56] == 'u'){
+								res.json({success: false, message: 'Username already exists'});
+							} else if(err.errmsg[55] == 'e'){
+								res.json({success: false, message: 'Email already exists'});
+							} else {
+								res.json({success: false, message: err.errmsg + ' You should not see this. Check the indexing with table name'});
+							}		
+						} else {
+							res.json({success: false, message: err});
+						}
+					} 
 				} else {
-					res.send('User created. Nice.')
+					// Success
+					res.json({success: true, message: 'User created. Nice!'});
 				}
 			});
 		}
 	});
+
+	// USER LOGIN ROUTE
+	// Provides JSON webtoken
+	// http://localhost:8080/api/authenticate
+	router.post('/authenticate', function(req, res){
+		// Grab user from DB
+		User.findOne({ username: req.body.username })
+		.select('email username password')
+		.exec(function(err, user){
+			//DB error check
+			if(err) throw err;
+			//auth -> compare to DB and see if they exit
+			if(!user){
+				res.json({ success: false, message: 'Could not authenticate user. Make sure you typed your name in correctly' });
+			} else {
+				// password present?
+				if(req.body.password){
+					var validPassword = user.comparePasswords(req.body.password);
+				} else {
+					res.json({success: false, message: 'No password provided'});
+				}
+				// password valid?
+				if(!validPassword){
+					res.json({success: false, message: 'Incorrect password' });
+				} else {
+					// Password is valid, create the web token and send everything back
+					var token = jwt.sign({
+						username: user.username, 
+						email: user.email 
+					}, secret, {expiresIn: '24h'});
+					res.json({success: true, message: 'User logged in successfully', token: token });
+				}
+			}
+
+		});
+
+	});
+
+
+	// GET CURRENT USER
+	// https://localhost:8080/api/me
+
+	// Middleware for getting and 
+	router.use(function(req, res, next) {
+		var token = req.body.token || req.body.query || req.headers['x-access-token'];
+		if(token){
+			jwt.verify(token, secret, function(err, decoded){
+				if(err) {
+					res.json({success: false, message: 'Token invalid'});
+				} else {
+					req.decoded = decoded;
+					next();
+				}
+
+			})
+		} else {
+			res.json({success: false, message: 'No token provided'});
+		}
+	});
+
+	// Current user route
+	router.post('/me', function(req, res){
+		res.send(req.decoded);
+	});
+
 	return router;
 }
