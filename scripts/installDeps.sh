969d54a5d3cf32160f543c89ef5289f96a672ec0
dylanhobbs <dhobbs@tcd.ie>
2018-02-26 21:08:56 +0000

    Install scripts

---
+++ scripts/installDeps.sh
@@ -0,0 +1,6 @@
+echo "Installing deps"
+sudo apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+sudo apt-get install -y git
